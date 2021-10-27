da243935f628f1df3991e474e5b35b817ecb4f09
tal ben-avi <tal@ben-avi.com>
2021-10-27 13:09:11 +0300

    render pics

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 'use strict'
+var gElCanvas;
+var gCtx;
+
+var gCurrImgUrl;
+var gCurrHeight;
+
+var gIsUpdating = false;
+var gDontGrab = false;
+
+var gCurrFont;
+
+var gStartPos;
 function onInit(){
     console.log('hi');
+    var imgs=loadImgs();
+    renderImgGallery(imgs);
+
+}
+function renderImgGallery(imgs) {
+    var strHTML = imgs.map((img) => {
+        return `<div class="img-container">
+        <img src="${img.url}" alt="">
+        </div>`
+    }).join('');
+
+    document.querySelector('.gallery').innerHTML = strHTML;
 }
